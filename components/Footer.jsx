67dcfc974b75c6457d00cd945be90056b2af200f
Joshhortt <joshhortt@yahoo.com>
2022-02-26 15:22:38 +0000

    add cell number to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/layout';
 
 const Footer = () => (
   <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
-    © 2022 home&home, Inc. | Created by Creativelightbox.com
+    © 2022 home&home, Inc. | Created by Creativelightbox.com | +351 911 815 877
   </Box>
 );
 
